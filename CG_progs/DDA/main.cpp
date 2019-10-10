da5fe386375a012d387422f5a3764ce1eac37a0c
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2019-10-10 20:20:48 +0530

    Add files via upload

---
+++ CG_progs/DDA/main.cpp
@@ -0,0 +1,36 @@
+#include<graphics.h>
+#include<iostream>
+#include<dos.h>
+#include<math.h>
+
+using namespace std;
+
+int main(){
+	
+	float x0,y0,x1,y1,dx,dy,steps;
+	int gd=DETECT, gm;
+	
+	initgraph(&gd, &gm, "");
+	
+	cout<<"Enter (x0,y0) , (x1,y1) \t";
+	cin>>x0>>y0>>x1>>y1;
+	
+	dx = x1-x0;
+	dy = y1-y0;
+	
+	steps = abs(dx)>=abs(dy) ? abs(dx):abs(dy);
+	
+	dx = dx/steps;
+	dy = dy/steps;
+	
+	for(int i=0; i<=steps; i++){
+		putpixel(x0,y0,4);
+		delay(50);
+		x0 += dx;
+		y0 += dy;
+	}
+	
+	getch();
+	closegraph();
+	return 0;
+}
