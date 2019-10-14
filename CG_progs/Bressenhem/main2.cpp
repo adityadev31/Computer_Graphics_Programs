499b8e072241b8d8f62289d1660a42b2227d9c7b
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2019-10-14 22:18:05 +0530

    Update main2.cpp

--- CG_progs/Bressenhem/main2.cpp
+++ CG_progs/Bressenhem/main2.cpp
@@ -1,76 +1,72 @@
-#include<graphics.h>
 #include<iostream>
+#include<graphics.h>
 #include<dos.h>
 #include<math.h>
-
 using namespace std;
 
 
 void lesser_m(int x0, int y0, int x1, int y1, int dx, int dy){
 	
-	int p0 = (2*dy)-dx;
+	int p0 = 2*dy - dx;
 	
-	while(x0!=x1 || y0!=y1){
+	while(x0!=x1 && y0!=y1){
 		
-		putpixel(x0, y0, RED);
-		delay(10);
+		putpixel(x0, y0, WHITE);
+		cout<<"("<<x0<<","<<y0<<") \n";
 		
 		if(p0<0){
-			x0 += 1;
-			p0 = p0 + (2*dy);
+			x0++;
+			p0 = p0 + 2*dy;
 		}
 		else{
-			x0 += 1;
-			y0 += 1;
+			x0++;
+			y0++;
 			p0 = p0 + 2*(dy-dx);
 		}
 	}
-	putpixel(x0, y0, RED);
-	
+	putpixel(x0, y0, WHITE);
+	cout<<"("<<x0<<","<<y0<<") \n";
 }
+
 
 
 void greater_m(int x0, int y0, int x1, int y1, int dx, int dy){
 	
-	int p0 = (2*dx)-dy;
+	int p0 = 2*dx - dy;
 	
-	while(x0!=x1 || y0!=y1){
+	while(x0!=x1 && y0!=y1){
 		
-		putpixel(x0, y0, RED);
-		delay(10);
+		putpixel(x0, y0, WHITE);
+		cout<<"("<<x0<<","<<y0<<") \n";
 		
 		if(p0<0){
-			y0 += 1;
-			p0 = p0 + (2*dx);
+			y0++;
+			p0 = p0 + 2*dx;
 		}
 		else{
-			x0 += 1;
-			y0 += 1;
+			x0++;
+			y0++;
 			p0 = p0 + 2*(dx-dy);
 		}
 	}
-	putpixel(x0, y0, RED);
-	
+	putpixel(x0, y0, WHITE);
+	cout<<"("<<x0<<","<<y0<<") \n";
 }
-
 
 
 
 main(){
 	
-	int x0=200, y0=300, x1=1000, y1=800, dx, dy;
-	int gd = DETECT, gm;
-	
-	initgraph(&gd, &gm, "");
+	int x0=2, y0=4, x1=10, y1=7, dx, dy, gd=DETECT, gm;
 	
 	dx = x1-x0;
 	dy = y1-y0;
-	
 	float m = float(dy)/float(dx);
 	
-	(m<1)?(lesser_m(x0, y0, x1, y1, dx, dy)):(greater_m(x0, y0, x1, y1, dx, dy));
-		
+	initgraph(&gd, &gm, "");
+	
+	(m<1) ? lesser_m(x0, y0, x1, y1, dx, dy) : greater_m(x0, y0, x1, y1, dx, dy);
+	
 	getch();
 	closegraph();
-	
 }
